03f6b8320d28f73f8e0b912861d3f6019f925eb5
Veda Bhaskara Ramanth Addala <vedabhaskararamanth.addala@factset.com>
2021-06-30 14:58:01 +0530

    fix(python) : adding lower version

--- python/fds.protobuf.stach.v2/setup.py
+++ python/fds.protobuf.stach.v2/setup.py
@@ -7,6 +7,6 @@ setup(name='fds.protobuf.stach.v2',
       author='analytics-reporting',
       author_email='analytics.api.support@factset.com',
       license='Apache License 2.0',
-      install_requires=["protobuf<4.0.0"],
+      install_requires=["protobuf>=3.12.0,<4.0.0"],
       packages=find_packages(),
       zip_safe=False)
