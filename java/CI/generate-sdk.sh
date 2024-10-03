acaab7b4246b5e7452136a27245ece85a36a23ca
Ramanth Addala <vedabhaskararamanth.addala@factset.com>
2024-10-03 11:17:38 +0530

    feat(sdk) : support for multiple versions in java  (#23)

--- java/CI/generate-sdk.sh
+++ java/CI/generate-sdk.sh
@@ -4,11 +4,31 @@ set -e
 BASE_PATH=/java # Base location of Java generated classes
 PACKAGE_PATH=src/main/java
 
-rm -f $BASE_PATH/$PACKAGE_PATH/**/*.java
+BASE_PATH_v1 = /java/v1
+BASE_PATH_v2 = /java/v2
+BASE_PATH_v3 = /java/v3
+
+rm -f $BASE_PATH/**/*.java
+
+
+mkdir -P $BASE_PATH_v1/$PACKAGE_PATH
+mkdir -P $BASE_PATH_v2/$PACKAGE_PATH
+mkdir -P $BASE_PATH_v3/$PACKAGE_PATH
 
 echo Removed old generated code
 
-cd $BASE_PATH
+# cd $BASE_PATH
+# mvn clean install -q -P generate-stach-schema -f pom.xml
+
+cd $BASE_PATH_v1
+mvn clean install -q -P generate-stach-schema -f pom.xml
+
+cd ../..
+cd $BASE_PATH_v2
+mvn clean install -q -P generate-stach-schema -f pom.xml
+
+cd ../..
+cd $BASE_PATH_v3
 mvn clean install -q -P generate-stach-schema -f pom.xml
 
 echo Produced new generated code
