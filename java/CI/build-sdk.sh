70572e0cf86687e806613740d44c2600d4d20498
Veda Bhaskara Ramanth Addala <vedabhaskararamanth.addala@factset.com>
2024-10-08 17:09:08 +0530

    fix(sdk) : removing redundent v3 code files

--- java/CI/build-sdk.sh
+++ java/CI/build-sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-BASE_PATH=/src # Base location of Java generated classes
+BASE_PATH=/src/v3 # Base location of Java generated classes
 
 cd $BASE_PATH
 mvn clean install -q -f pom.xml
