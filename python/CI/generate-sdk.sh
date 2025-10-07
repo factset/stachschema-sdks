89b3943e916609ff76560fd5b156617fe16a20cd
Akanksha Acharya <akanksha4510471@gmail.com>
2025-10-07 12:02:16 +0530

    Update generate-sdk.sh

--- python/CI/generate-sdk.sh
+++ python/CI/generate-sdk.sh
@@ -14,14 +14,14 @@ rm -f $BASE_PATH_V3/$PACKAGE_PATH_V3/**/*_pb2.py
 mkdir -p "$BASE_PATH_V3"
 mkdir -p "$BASE_PATH_V2"
 
-PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V3 -iname "*.proto")
+PROTOFILES=$(find $SCHEMA_PATH/v3 -iname "*.proto")
 protoc --proto_path $SCHEMA_PATH/v3 --python_out $BASE_PATH_V3 $PROTOFILES
 
 for f in $(find $BASE_PATH_V3/fds/protobuf/stach/v3 -type d -maxdepth 10); do
 	touch "$f/__init__.py"
 done;
 
-PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V2 -iname "*.proto")
+PROTOFILES=$(find $SCHEMA_PATH/v2 -iname "*.proto")
 protoc --proto_path $SCHEMA_PATH/v2 --python_out $BASE_PATH_V2 $PROTOFILES
 
 for f in $(find $BASE_PATH_V2/fds/protobuf/stach/v2 -type d -maxdepth 10); do
