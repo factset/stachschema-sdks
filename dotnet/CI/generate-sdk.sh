acc5d894626c1e1e54996bd3e0e3549422834279
Andrii Lopatin <fds45678@pc.factset.com>
2026-01-07 11:33:45 +0000

    feat(dotnet): add v1 and v2 support

--- dotnet/CI/generate-sdk.sh
+++ dotnet/CI/generate-sdk.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
 
+BASE_PATH_V1=/dotnet/v1/Models # Base location of dotnet generated classes for v1
+BASE_PATH_V2=/dotnet/v2/Models # Base location of dotnet generated classes for v2
+BASE_PATH_V3=/dotnet/v3/Models # Base location of dotnet generated classes for v3
+
 SCHEMA_PATH=/schema # Location of Protobuf schema
-BASE_PATH=/dotnet/Schema # Base location of C# generated classes
-PACKAGE_PATH=fds/protobuf/stach
+PACKAGE_PATH_V1=v1/fds/protobuf/stach
+PACKAGE_PATH_V2=v2/fds/protobuf/stach/v2
+PACKAGE_PATH_V3=v3/fds/protobuf/stach/v3
 
-rm -f $BASE_PATH/$PACKAGE_PATH/**/*.g.cs
+# echo Removed old generated code
+find $BASE_PATH_V1 -name *.g.cs -type f -delete
+find $BASE_PATH_V2 -name *.g.cs -type f -delete
+find $BASE_PATH_V3 -name *.g.cs -type f -delete
 
-echo Removed old generated code
+PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V1 -iname "*.proto")
+protoc --proto_path $SCHEMA_PATH/v1 --csharp_out $BASE_PATH_V1 --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach $PROTOFILES
 
-PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH -iname "*.proto")
-protoc --proto_path $SCHEMA_PATH --csharp_out $BASE_PATH/$PACKAGE_PATH --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach.V3 $PROTOFILES
+PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V2 -iname "*.proto")
+protoc --proto_path $SCHEMA_PATH/v2 --csharp_out $BASE_PATH_V2 --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach.V2 $PROTOFILES
+
+PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V3 -iname "*.proto")
+protoc --proto_path $SCHEMA_PATH/v3 --csharp_out $BASE_PATH_V3 --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach.V3 $PROTOFILES
 
 echo Produced new generated code
