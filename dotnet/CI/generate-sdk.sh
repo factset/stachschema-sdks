405bed9779a88b8069e52568d3079d259e1d0712
Andrii Lopatin <fds45678@pc.factset.com>
2026-01-08 13:38:21 +0000

    cleanup

--- dotnet/CI/generate-sdk.sh
+++ dotnet/CI/generate-sdk.sh
@@ -11,18 +11,12 @@ PACKAGE_PATH_V1=v1/fds/protobuf/stach
 PACKAGE_PATH_V2=v2/fds/protobuf/stach/v2
 PACKAGE_PATH_V3=v3/fds/protobuf/stach/v3
 
-echo Start removing old generated code
-
 find "$BASE_PATH_V1" -name '*.g.cs' -type f -delete
 find "$BASE_PATH_V2" -name '*.g.cs' -type f -delete
 find "$BASE_PATH_V3" -name '*.g.cs' -type f -delete
 
-echo Removed old generated code
-
-echo Start adding proto v1
 PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V1 -iname "*.proto")
 protoc --proto_path $SCHEMA_PATH/v1 --csharp_out $BASE_PATH_V1 --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach $PROTOFILES
-echo Added proto v1
 
 PROTOFILES=$(find $SCHEMA_PATH/$PACKAGE_PATH_V2 -iname "*.proto")
 protoc --proto_path $SCHEMA_PATH/v2 --csharp_out $BASE_PATH_V2 --csharp_opt=file_extension=.g.cs,base_namespace=FactSet.Protobuf.Stach.V2 $PROTOFILES
